0f93962e602867490b37603012d716da4d30c716
Khalid Salomão <khalidsalomao@gmail.com>
2015-04-15 15:40:21 -0300

    Web: pages review

---
+++ BigDataPipeline.Web/Controllers/ConfigurationModule.cs
@@ -0,0 +1,50 @@
+﻿using BigDataPipeline.Core;
+using BigDataPipeline.Web.Models;
+using Nancy;
+using Nancy.Extensions;
+using Nancy.Security;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BigDataPipeline.Interfaces;
+
+namespace BigDataPipeline.Web.site.Controllers
+{
+    public class ConfigurationModule : NancyModule
+    {
+        public ConfigurationModule () : base ("/Configuration")
+        {
+            this.RequiresAuthentication ();
+
+            Get["/"] = p => View["Index"];
+
+            Post["/query"] = QueryEvents;
+        }
+
+        private dynamic QueryEvents (dynamic p)
+        {
+            var item = this.Bind<QueryRequest> ();
+
+            if (item == null || item.op == null)
+                return new QueryResponse (false, "Invalid parameters");
+
+            switch (item.op.ToLowerInvariant ())
+            {
+                case "load":
+                    {                        
+                        return new QueryResponse (true)
+                        {
+                            result = new Dictionary<string,string>()
+                        };
+                    }                
+                default:
+                    return new QueryResponse (false, "Invalid parameters");
+            }
+            //return Response.AsText ("");
+        }
+
+    }
+}
